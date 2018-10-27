b8555a9311e05d675dde50c77c855a1c978c7c2d
lufonius <fonjallaz97@gmail.com>
2018-10-27 15:42:31 +0200

    changed structure to have separate folders for the store and components, now bundling with ng-packagr, added stages to jenkinsfile #22 #21

--- dev-env/src/app/app.module.ts
+++ dev-env/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UserManagementModule } from '@base-app-toolbox/user-management';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserManagementModule
   ],
   providers: [],
   bootstrap: [AppComponent]
